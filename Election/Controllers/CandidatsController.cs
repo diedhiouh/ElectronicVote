2545db9a872edf32ec53857fa392c384d651d0a5
Habb=ibou DIEDHIOU <habibodh@gmail.com>
2021-02-15 09:46:28 +0100

    Finalisation de la plateforme

--- Election/Controllers/CandidatsController.cs
+++ Election/Controllers/CandidatsController.cs
@@ -44,6 +44,7 @@ namespace Election.Controllers
         // POST: Candidats/Create
         // Afin de déjouer les attaques par survalidation, activez les propriétés spécifiques auxquelles vous voulez établir une liaison. Pour 
         // plus de détails, consultez https://go.microsoft.com/fwlink/?LinkId=317598.
+
         [HttpPost]
         [ValidateAntiForgeryToken]
         public ActionResult Create([Bind(Include = "prenom,nom,parti,datenaiss,photo,voix")] Candidat candidat)
